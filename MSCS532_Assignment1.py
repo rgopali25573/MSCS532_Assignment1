4daa151afd3d4f9cc859b7d70f80c4f7cae2c732
Gopali, Rinku <rgopali25573@ucumberlands.edu>
2024-09-01 13:02:58 -0500

    added a comment

--- MSCS532_Assignment1.py
+++ MSCS532_Assignment1.py
@@ -1,4 +1,5 @@
 def insertion_sort_desc(array_list):
+    # traverse fron 1 to length of given array_list
     for i in range(1, len(array_list)):
         current_value = array_list[i]
         j = i - 1
@@ -6,9 +7,10 @@ def insertion_sort_desc(array_list):
             array_list[j + 1] = array_list[j]
             j -= 1
         array_list[j + 1] = current_value
+    return array_list   
 
 
 # Sample Example
-array_list = [1, 2, 3, 4, 5, 6, 8, 10, 12]
+array_list = [8, 2, 3, 4, 5, 6, 8, 10, 12]
 insertion_sort_desc(array_list)
-print(array_list)
+print("Sorted array list using Insertion Sort Algorithm in monotonically decreasing order:", array_list)
